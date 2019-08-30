c152b783ebb71cb653a7fc65c222347d5468be4a
gastonfeng <gastonfeng@gmail.com>
2019-08-30 17:33:39 +0800

    .

--- src/WebServer.cpp
+++ src/WebServer.cpp
@@ -21,82 +21,27 @@
 #include <Arduino.h>
 #include <LwIP.h>
 #include <STM32Ethernet.h>
-
+#include <core_debug.h>
+extern "C"
+{
+  void httpd_init(void);
+}
 // Enter an IP address for your controller below.
 // The IP address will be dependent on your local network:
 IPAddress ip(192, 168, 31, 156);
-HardwareSerial Serial1(USART1);
 // Initialize the Ethernet server library
 // with the IP address and port you want to use
 // (port 80 is default for HTTP):
 EthernetServer server(80);
 
-void httpd_setup() {
-  // Open serial communications and wait for port to open:
-  Serial1.begin(9600);
-  Serial1.print("server is start ");
-    // while (!Serial1) {
-  //   ; // wait for serial port to connect. Needed for native USB port only
-  // }
-
-
+void httpd_setup()
+{
   // start the Ethernet connection and the server:
   Ethernet.begin(ip);
-  server.begin();
-  Serial1.print("server is at ");
-  Serial1.println(Ethernet.localIP());
+  httpd_init();
 }
 
-
-void httpd_loop() {
+void httpd_loop()
+{
   stm32_eth_scheduler();
-  // listen for incoming clients
-  EthernetClient client = server.available();
-  if (client) {
-    Serial.println("new client");
-    // an http request ends with a blank line
-    bool currentLineIsBlank = true;
-    while (client.connected()) {
-      if (client.available()) {
-        char c = client.read();
-        Serial1.write(c);
-        // if you've gotten to the end of the line (received a newline
-        // character) and the line is blank, the http request has ended,
-        // so you can send a reply
-        if (c == '\n' && currentLineIsBlank) {
-          // send a standard http response header
-          client.println("HTTP/1.1 200 OK");
-          client.println("Content-Type: text/html");
-          client.println("Connection: close");  // the connection will be closed after completion of the response
-          client.println("Refresh: 5");  // refresh the page automatically every 5 sec
-          client.println();
-          client.println("<!DOCTYPE HTML>");
-          client.println("<html>");
-          // output the value of each analog input pin
-          for (int analogChannel = 0; analogChannel < 6; analogChannel++) {
-            int sensorReading = analogRead(analogChannel);
-            client.print("analog input ");
-            client.print(analogChannel);
-            client.print(" is ");
-            client.print(sensorReading);
-            client.println("<br />");
-          }
-          client.println("</html>");
-          break;
-        }
-        if (c == '\n') {
-          // you're starting a new line
-          currentLineIsBlank = true;
-        } else if (c != '\r') {
-          // you've gotten a character on the current line
-          currentLineIsBlank = false;
-        }
-      }
-    }
-    // give the web browser time to receive the data
-    delay(1);
-    // close the connection:
-    client.stop();
-    Serial1.println("client disconnected");
-  }
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
 #include <Arduino.h>
 #include <lwip/apps/httpd.h>
 #include <core_debug.h>
-
+#include <uart.h>
 // #include "plc.h"
 //#include <plusein.h>
 //#include <ArduinoRS485.h> // ArduinoModbus depends on the ArduinoRS485 library
@@ -11,6 +11,8 @@ const char swname[] = "1260orig";
 uint8_t xx1, xx2, xx3, xx4, xx5, xx6, xx7, xx8, xx9, xx10, xx11, xx12, xx13, xx14, xx15, xx16, yy1, yy2, yy3, yy4, yy5, yy6, yy7, yy8, yy9, yy10, yy11, yy12, yy13, yy14, yy15, yy16, yy17, yy18, yy19, yy20, yy21, yy22, yy23, yy24;
 uint16_t xa1, xa2, xa3, xa4, xa5, xa6, xa7, xa8, xa9, xa10, ya1, ya2;
 int xt1, xt2, xt3, xt4;
+
+HardwareSerial Serial1(USART1);
 
 #include <ArduinoModbus.h>
 #include <ArduinoRS485.h>
@@ -45,14 +47,10 @@ void httpd_setup();
 void httpd_loop();
 extern "C"
 {
+    void uart_debug_init(void);
     void phy_dump();
     void config_init__();
     void config_run__(unsigned long tick);
-    int debug_printf(const char *str, ...)
-    {
-        Serial1.println(str);
-        return 0;
-    }
 
     unsigned int GetRand()
     {
@@ -63,7 +61,8 @@ extern "C"
 
 void setup()
 {
-        usb.begin();
+    uart_debug_init();
+    usb.begin();
 
     sr.setAllLow();
     pinMode(PC12, OUTPUT);
@@ -140,167 +139,167 @@ int last;
 void loop()
 {
     uptime = millis() / 60000;
-    avref = analogRead(AVREF);
-    atemp = analogRead(ATEMP) - atemp_init;
-    // Serial1.print(pcf8563.formatDate());
-    // Serial1.print(" ");
-    // Serial1.println(pcf8563.formatTime());
-    yy1 = ms2.coilRead(0);
-    yy2 = ms2.coilRead(1);
-    yy3 = ms2.coilRead(2);
-    yy4 = ms2.coilRead(3);
-    yy5 = ms2.coilRead(4);
-    yy6 = ms2.coilRead(5);
-    yy7 = ms2.coilRead(6);
-    yy8 = ms2.coilRead(7);
-    yy9 = ms2.coilRead(8);
-    yy10 = ms2.coilRead(9);
-    yy11 = ms2.coilRead(10);
-    yy12 = ms2.coilRead(11);
-    yy13 = ms2.coilRead(12);
-    yy14 = ms2.coilRead(13);
-    yy15 = ms2.coilRead(14);
-    yy16 = ms2.coilRead(15);
-    yy17 = ms2.coilRead(16);
-    yy18 = ms2.coilRead(17);
-    yy19 = ms2.coilRead(18);
-    yy20 = ms2.coilRead(19);
-    yy21 = ms2.coilRead(20);
-    yy22 = ms2.coilRead(21);
-    yy23 = ms2.coilRead(22);
-    yy24 = ms2.coilRead(23);
-    ya1 = ms2.holdingRegisterRead(2);
-    ya2 = ms2.holdingRegisterRead(3);
-    xa1 = ad.acquireChannel(0) * 2000 / 2.5;
-    xa2 = ad.acquireChannel(1) * 2000 / 2.5;
-    xa3 = ad.acquireChannel(2) * 2000 / 2.5;
-    xa4 = analogRead(XA4);
-    xa5 = ad.acquireChannel(3) * 2000 / 2.5;
-    xa6 = ad.acquireChannel(4) * 2000 / 2.5;
-    xa7 = analogRead(XA7);
-    xa8 = ad.acquireChannel(5) * 2000 / 2.5;
-    xa9 = ad.acquireChannel(6) * 2000 / 2.5;
-    xa10 = ad.acquireChannel(7) * 2000 / 2.5;
-    xx1 = digitalRead(X1);
-    xx2 = digitalRead(X2);
-    xx3 = digitalRead(X3);
-    xx4 = digitalRead(X4);
-    xx5 = digitalRead(X5);
-    xx6 = digitalRead(X6);
-    xx7 = digitalRead(X7);
-    xx8 = digitalRead(X8);
-    xx9 = digitalRead(X9);
-    xx10 = digitalRead(X10);
-    xx11 = digitalRead(X11);
-    xx12 = digitalRead(X12);
-    xx13 = digitalRead(X13);
-    xx14 = digitalRead(X14);
-    xx15 = digitalRead(X15);
-    xx16 = digitalRead(X16);
-    // hx1.loop();
-    // hx2.loop();
-    // hx3.loop();
-    // hx4.loop();
-    xt1 = hx1.value() * 100;
-    xt2 = hx2.value() * 100;
-    xt3 = hx3.value() * 100;
-    xt4 = hx4.value() * 100;
-    if (millis() - last > 10000)
-    {
-        // phy_dump();
-        last = millis();
-    }
+    // avref = analogRead(AVREF);
+    // atemp = analogRead(ATEMP) - atemp_init;
+    // // Serial1.print(pcf8563.formatDate());
+    // // Serial1.print(" ");
+    // // Serial1.println(pcf8563.formatTime());
+    // yy1 = ms2.coilRead(0);
+    // yy2 = ms2.coilRead(1);
+    // yy3 = ms2.coilRead(2);
+    // yy4 = ms2.coilRead(3);
+    // yy5 = ms2.coilRead(4);
+    // yy6 = ms2.coilRead(5);
+    // yy7 = ms2.coilRead(6);
+    // yy8 = ms2.coilRead(7);
+    // yy9 = ms2.coilRead(8);
+    // yy10 = ms2.coilRead(9);
+    // yy11 = ms2.coilRead(10);
+    // yy12 = ms2.coilRead(11);
+    // yy13 = ms2.coilRead(12);
+    // yy14 = ms2.coilRead(13);
+    // yy15 = ms2.coilRead(14);
+    // yy16 = ms2.coilRead(15);
+    // yy17 = ms2.coilRead(16);
+    // yy18 = ms2.coilRead(17);
+    // yy19 = ms2.coilRead(18);
+    // yy20 = ms2.coilRead(19);
+    // yy21 = ms2.coilRead(20);
+    // yy22 = ms2.coilRead(21);
+    // yy23 = ms2.coilRead(22);
+    // yy24 = ms2.coilRead(23);
+    // ya1 = ms2.holdingRegisterRead(2);
+    // ya2 = ms2.holdingRegisterRead(3);
+    // xa1 = ad.acquireChannel(0) * 2000 / 2.5;
+    // xa2 = ad.acquireChannel(1) * 2000 / 2.5;
+    // xa3 = ad.acquireChannel(2) * 2000 / 2.5;
+    // xa4 = analogRead(XA4);
+    // xa5 = ad.acquireChannel(3) * 2000 / 2.5;
+    // xa6 = ad.acquireChannel(4) * 2000 / 2.5;
+    // xa7 = analogRead(XA7);
+    // xa8 = ad.acquireChannel(5) * 2000 / 2.5;
+    // xa9 = ad.acquireChannel(6) * 2000 / 2.5;
+    // xa10 = ad.acquireChannel(7) * 2000 / 2.5;
+    // xx1 = digitalRead(X1);
+    // xx2 = digitalRead(X2);
+    // xx3 = digitalRead(X3);
+    // xx4 = digitalRead(X4);
+    // xx5 = digitalRead(X5);
+    // xx6 = digitalRead(X6);
+    // xx7 = digitalRead(X7);
+    // xx8 = digitalRead(X8);
+    // xx9 = digitalRead(X9);
+    // xx10 = digitalRead(X10);
+    // xx11 = digitalRead(X11);
+    // xx12 = digitalRead(X12);
+    // xx13 = digitalRead(X13);
+    // xx14 = digitalRead(X14);
+    // xx15 = digitalRead(X15);
+    // xx16 = digitalRead(X16);
+    // // hx1.loop();
+    // // hx2.loop();
+    // // hx3.loop();
+    // // hx4.loop();
+    // xt1 = hx1.value() * 100;
+    // xt2 = hx2.value() * 100;
+    // xt3 = hx3.value() * 100;
+    // xt4 = hx4.value() * 100;
+    // if (millis() - last > 10000)
+    // {
+    //     // phy_dump();
+    //     last = millis();
+    // }
     // config_run__(10);
     httpd_loop();
-    sr.setNoUpdate(0, yy1);
-    sr.setNoUpdate(1, yy2);
-    sr.setNoUpdate(2, yy3);
-    sr.setNoUpdate(3, yy4);
-    sr.setNoUpdate(4, yy5);
-    sr.setNoUpdate(5, yy6);
-    sr.setNoUpdate(6, yy7);
-    sr.setNoUpdate(7, yy8);
-    sr.setNoUpdate(8, yy9);
-    sr.setNoUpdate(9, yy10);
-    sr.setNoUpdate(10, yy11);
-    sr.setNoUpdate(11, yy12);
-    sr.setNoUpdate(12, yy13);
-    sr.setNoUpdate(13, yy14);
-    sr.setNoUpdate(14, yy15);
-    sr.setNoUpdate(15, yy16);
-    sr.setNoUpdate(16, yy17);
-    sr.setNoUpdate(17, yy18);
-    sr.setNoUpdate(18, yy19);
-    sr.setNoUpdate(19, yy20);
-    sr.setNoUpdate(20, yy21);
-    sr.setNoUpdate(21, yy22);
-    sr.setNoUpdate(22, yy23);
-    sr.setNoUpdate(23, yy24);
-    sr.updateRegisters();
-    mcp.setVoltage(0, ya1);
-    mcp.setVoltage(1, ya2);
-    mcp.update();
-    // ch.write();
-    ms2.discreteInputWrite(0, xx1);
-    ms2.discreteInputWrite(1, xx2);
-    ms2.discreteInputWrite(2, xx3);
-    ms2.discreteInputWrite(3, xx4);
-    ms2.discreteInputWrite(4, xx5);
-    ms2.discreteInputWrite(5, xx6);
-    ms2.discreteInputWrite(6, xx7);
-    ms2.discreteInputWrite(7, xx8);
-    ms2.discreteInputWrite(8, xx9);
-    ms2.discreteInputWrite(9, xx10);
-    ms2.discreteInputWrite(10, xx11);
-    ms2.discreteInputWrite(11, xx12);
-    ms2.discreteInputWrite(12, xx13);
-    ms2.discreteInputWrite(13, xx14);
-    ms2.discreteInputWrite(14, xx15);
-    ms2.discreteInputWrite(15, xx16);
-    ms2.inputRegisterWrite(0, xa1);
-    ms2.inputRegisterWrite(1, xa2);
-    ms2.inputRegisterWrite(2, xa3);
-    ms2.inputRegisterWrite(3, xa4);
-    ms2.inputRegisterWrite(4, xa5);
-    ms2.inputRegisterWrite(5, xa6);
-    ms2.inputRegisterWrite(6, xa7);
-    ms2.inputRegisterWrite(7, xa8);
-    ms2.inputRegisterWrite(8, xa9);
-    ms2.inputRegisterWrite(9, xa10);
-    ms2.inputRegisterWrite(10, xt1);
-    ms2.inputRegisterWrite(11, xt2);
-    ms2.inputRegisterWrite(12, xt3);
-    ms2.inputRegisterWrite(13, xt4);
-    ms2.coilWrite(0, yy1);
-    ms2.coilWrite(1, yy2);
-    ms2.coilWrite(2, yy3);
-    ms2.coilWrite(3, yy4);
-    ms2.coilWrite(4, yy5);
-    ms2.coilWrite(5, yy6);
-    ms2.coilWrite(6, yy7);
-    ms2.coilWrite(7, yy8);
-    ms2.coilWrite(8, yy9);
-    ms2.coilWrite(9, yy10);
-    ms2.coilWrite(10, yy11);
-    ms2.coilWrite(11, yy12);
-    ms2.coilWrite(12, yy13);
-    ms2.coilWrite(13, yy14);
-    ms2.coilWrite(14, yy15);
-    ms2.coilWrite(15, yy16);
-    ms2.coilWrite(16, yy17);
-    ms2.coilWrite(17, yy18);
-    ms2.coilWrite(18, yy19);
-    ms2.coilWrite(19, yy20);
-    ms2.coilWrite(20, yy21);
-    ms2.coilWrite(21, yy22);
-    ms2.coilWrite(22, yy23);
-    ms2.coilWrite(23, yy24);
-    ms2.holdingRegisterWrite(0, build);  //
-    ms2.holdingRegisterWrite(1, uptime); //
-    ms2.holdingRegisterWrite(2, ya1);    //
-    ms2.holdingRegisterWrite(3, ya2);    //
-    ms2.poll();
-    usb.loop();
+    // sr.setNoUpdate(0, yy1);
+    // sr.setNoUpdate(1, yy2);
+    // sr.setNoUpdate(2, yy3);
+    // sr.setNoUpdate(3, yy4);
+    // sr.setNoUpdate(4, yy5);
+    // sr.setNoUpdate(5, yy6);
+    // sr.setNoUpdate(6, yy7);
+    // sr.setNoUpdate(7, yy8);
+    // sr.setNoUpdate(8, yy9);
+    // sr.setNoUpdate(9, yy10);
+    // sr.setNoUpdate(10, yy11);
+    // sr.setNoUpdate(11, yy12);
+    // sr.setNoUpdate(12, yy13);
+    // sr.setNoUpdate(13, yy14);
+    // sr.setNoUpdate(14, yy15);
+    // sr.setNoUpdate(15, yy16);
+    // sr.setNoUpdate(16, yy17);
+    // sr.setNoUpdate(17, yy18);
+    // sr.setNoUpdate(18, yy19);
+    // sr.setNoUpdate(19, yy20);
+    // sr.setNoUpdate(20, yy21);
+    // sr.setNoUpdate(21, yy22);
+    // sr.setNoUpdate(22, yy23);
+    // sr.setNoUpdate(23, yy24);
+    // sr.updateRegisters();
+    // mcp.setVoltage(0, ya1);
+    // mcp.setVoltage(1, ya2);
+    // mcp.update();
+    // // ch.write();
+    // ms2.discreteInputWrite(0, xx1);
+    // ms2.discreteInputWrite(1, xx2);
+    // ms2.discreteInputWrite(2, xx3);
+    // ms2.discreteInputWrite(3, xx4);
+    // ms2.discreteInputWrite(4, xx5);
+    // ms2.discreteInputWrite(5, xx6);
+    // ms2.discreteInputWrite(6, xx7);
+    // ms2.discreteInputWrite(7, xx8);
+    // ms2.discreteInputWrite(8, xx9);
+    // ms2.discreteInputWrite(9, xx10);
+    // ms2.discreteInputWrite(10, xx11);
+    // ms2.discreteInputWrite(11, xx12);
+    // ms2.discreteInputWrite(12, xx13);
+    // ms2.discreteInputWrite(13, xx14);
+    // ms2.discreteInputWrite(14, xx15);
+    // ms2.discreteInputWrite(15, xx16);
+    // ms2.inputRegisterWrite(0, xa1);
+    // ms2.inputRegisterWrite(1, xa2);
+    // ms2.inputRegisterWrite(2, xa3);
+    // ms2.inputRegisterWrite(3, xa4);
+    // ms2.inputRegisterWrite(4, xa5);
+    // ms2.inputRegisterWrite(5, xa6);
+    // ms2.inputRegisterWrite(6, xa7);
+    // ms2.inputRegisterWrite(7, xa8);
+    // ms2.inputRegisterWrite(8, xa9);
+    // ms2.inputRegisterWrite(9, xa10);
+    // ms2.inputRegisterWrite(10, xt1);
+    // ms2.inputRegisterWrite(11, xt2);
+    // ms2.inputRegisterWrite(12, xt3);
+    // ms2.inputRegisterWrite(13, xt4);
+    // ms2.coilWrite(0, yy1);
+    // ms2.coilWrite(1, yy2);
+    // ms2.coilWrite(2, yy3);
+    // ms2.coilWrite(3, yy4);
+    // ms2.coilWrite(4, yy5);
+    // ms2.coilWrite(5, yy6);
+    // ms2.coilWrite(6, yy7);
+    // ms2.coilWrite(7, yy8);
+    // ms2.coilWrite(8, yy9);
+    // ms2.coilWrite(9, yy10);
+    // ms2.coilWrite(10, yy11);
+    // ms2.coilWrite(11, yy12);
+    // ms2.coilWrite(12, yy13);
+    // ms2.coilWrite(13, yy14);
+    // ms2.coilWrite(14, yy15);
+    // ms2.coilWrite(15, yy16);
+    // ms2.coilWrite(16, yy17);
+    // ms2.coilWrite(17, yy18);
+    // ms2.coilWrite(18, yy19);
+    // ms2.coilWrite(19, yy20);
+    // ms2.coilWrite(20, yy21);
+    // ms2.coilWrite(21, yy22);
+    // ms2.coilWrite(22, yy23);
+    // ms2.coilWrite(23, yy24);
+    // ms2.holdingRegisterWrite(0, build);  //
+    // ms2.holdingRegisterWrite(1, uptime); //
+    // ms2.holdingRegisterWrite(2, ya1);    //
+    // ms2.holdingRegisterWrite(3, ya2);    //
+    // ms2.poll();
+    // usb.loop();
 }
 //DEFINE_USART_RS485(mons, 1, 0, 0);
 //DEFINE_USART_RS485(rs485, 3, 0, 0);
